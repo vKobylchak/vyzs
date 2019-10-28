fd5887ad0d099f62ab26c28a7a8af2f329a5bd19
vKobylchak <vkobylchak@corevalue.net>
2019-10-28 15:31:13 +0200

    update VyzApplication

--- src/main/java/ua/vyz/VyzApplication.java
+++ src/main/java/ua/vyz/VyzApplication.java
@@ -9,7 +9,9 @@ import ua.vyz.model.Vyz;
 import ua.vyz.repository.FacultetRepository;
 import ua.vyz.repository.VyzRepository;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class VyzApplication {
@@ -27,28 +29,20 @@ public class VyzApplication {
                 List<Facultet> facultets = new ArrayList<>();
                 facultets.add(botanu);
 
-                Facultet pacuku = new Facultet(2, "Пацани");
+                Facultet pacuku = new Facultet(2, "pacanu");
                 List<Facultet> facultetPacuku = new ArrayList<>();
                 facultetPacuku.add(pacuku);
 
                 facultetRepository.save(botanu);
                 facultetRepository.save(pacuku);
                 List<Vyz> vyzs = Arrays.asList(new Vyz(1, "MED", "Odessa", 120, facultets),
-                        new Vyz(2, "NAO", "Lviv", 130, facultets),
+                        new Vyz(2, "NAO", "Lviv", 120, facultets),
                         new Vyz(3, "SHMAO", "Kyiv", 130, facultets),
                         new Vyz(4, "PUT", "Kyiv", 140, facultetPacuku),
                         new Vyz(5, "KOC", "Lviv", 150, facultets),
                         new Vyz(6, "UNI", "Kharkiv", 180, facultets));
 
                 vyzs.forEach(vyzRepository::save);
-
-//                Vyz vyz = new Vyz(1, "MED", "Odessa", 120, facultets);
-//                Vyz vyz1 = new Vyz(1, "NAO", "Lviv", 120, facultets);
-//                Vyz vyz2 = new Vyz(1, "SHMAO", "Kyiv", 120, facultets);
-//                Vyz vyz3 = new Vyz(1, "PUT", "Kyiv", 120, facultets);
-//                Vyz vyz4 = new Vyz(1, "KOC", "Lviv", 120, facultets);
-//                Vyz vyz5 = new Vyz(1, "UNI", "Kharkiv", 120, facultets);
-//                vyzRepository.save(vyz);
 
             }
         };
