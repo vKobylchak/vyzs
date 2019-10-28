a76c090ecaeb30e6fbec7efdf107a13fe32ca6aa
vKobylchak <vkobylchak@corevalue.net>
2019-10-28 15:26:59 +0200

    add model files

--- src/main/java/ua/vyz/model/Facultet.java
+++ src/main/java/ua/vyz/model/Facultet.java
@@ -1,28 +1,24 @@
 package ua.vyz.model;
 
-import lombok.Data;
 import lombok.Getter;
 
-import javax.persistence.*;
-import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "facultets")
 public class Facultet {
-
     @Id
-//    @Column(name = "ID")
     private int id;
 
     private String title;
 
-//    @ManyToMany(mappedBy = "facultets")
-//    Set<Vyz> vyzes;
-
-    @ManyToOne
-    @JoinColumn(name = "vyz_id")
-    private Vyz vyz;
+    @ManyToMany(mappedBy = "facultets")
+    List<Vyz> vyzs;
 
     public Facultet(int id, String title) {
         this.id = id;
